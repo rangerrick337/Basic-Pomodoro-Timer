15343a1795a56fc488496e4c249f32cb67d90efb
rangerrick337 <leerick@gmail.com>
2016-11-30 21:44:26 -0800

    Fixed missing second bug and added reset button
    reset button had to use clock.stop() which has the callbacks to play
    the sound and pop an alert. something to fix later.

--- js/index.js
+++ js/index.js
@@ -1,30 +1,47 @@
 $(document).ready(function() {
 
-  var clock = $('.your-clock').FlipClock(10,{
+  var MINUTE = 60;
+  var DEFAULT_SECONDS = 1500;  // sets 25 minutes to the clock by default
+  var audio = new Audio('audio/sound.mp3');
+
+  var clock = $('.your-clock').FlipClock(DEFAULT_SECONDS,{
     clockFace: 'MinuteCounter',
     countdown: true,
     autoStart: false,
     callbacks: {
+      /*
+      * the stop callback is called at the start of the last animation,
+      * the setTimeout method below waits for the time of the last animation
+      * to finish before firing the alert, a hack but it works
+      */
+
       stop: function() {
-        alert("Time's up yo!");
+        setTimeout(function() {
+            audio.play();
+            alert("Time's up yo!");
+          }, clock.time.animationRate);
+        },
       },
-    },
-    
-  });
-  
-  console.log(clock);
+    });
 
   $("#start").on('click', function() {
     clock.start();
   });
 
   $("#addTime").on('click', function() {
-    clock.time.addSeconds(61);
+    clock.time.addSeconds(MINUTE);
     clock.setTime(clock.time.time);
+    clock.time.addSecond();
   });
-  
+
   $('#subtractTime').click(function() {
-    clock.time.subSeconds(59);
+    clock.time.subSeconds(MINUTE);
     clock.setTime(clock.time.time);
+    clock.time.addSecond();
+  });
+
+  $('#reset').click(function() {
+    clock.stop();
+    clock.setTime(DEFAULT_SECONDS);
   });
 });
